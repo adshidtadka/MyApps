eb8cedec982bc1e9a314876c7a9d5ed81a101918
adshidtadka <tsawa@icn.cce.i.kyoto-u.ac.jp>
2018-10-19 12:21:41 +0900

    initial commit

---
+++ MyCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCalc
+//
+//  Created by Sawa Takaaki on 2018/10/19.
+//  Copyright © 2018年 Sawa Takaaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
