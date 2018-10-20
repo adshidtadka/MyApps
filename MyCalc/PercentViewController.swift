f6441a45ed90d95aa1a3542428bf94bf663a81f8
adshidtadka <tsawa@icn.cce.i.kyoto-u.ac.jp>
2018-10-21 00:33:27 +0900

    add number button

--- MyCalc/PercentViewController.swift
+++ MyCalc/PercentViewController.swift
@@ -22,8 +22,79 @@ class PercentViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func tap1Button(_ sender: Any) {
+        let value = percentField.text! + "1"
+        if let percent = Int(value) {
+            percentField.text = "\(percent)"
+        }
+    }
     
+    @IBAction func tap2Button(_ sender: Any) {
+        let value = percentField.text! + "2"
+        if let percent = Int(value) {
+            percentField.text = "\(percent)"
+        }
+    }
     
+    @IBAction func tap3Button(_ sender: Any) {
+        let value = percentField.text! + "3"
+        if let percent = Int(value) {
+            percentField.text = "\(percent)"
+        }
+    }
+    
+    @IBAction func tap4Button(_ sender: Any) {
+        let value = percentField.text! + "4"
+        if let percent = Int(value) {
+            percentField.text = "\(percent)"
+        }
+    }
+    
+    @IBAction func tap5Button(_ sender: Any) {
+        let value = percentField.text! + "5"
+        if let percent = Int(value) {
+            percentField.text = "\(percent)"
+        }
+    }
+    
+    @IBAction func tap6Button(_ sender: Any) {
+        let value = percentField.text! + "6"
+        if let percent = Int(value) {
+            percentField.text = "\(percent)"
+        }
+    }
+    
+    @IBAction func tap7Button(_ sender: Any) {
+        let value = percentField.text! + "7"
+        if let percent = Int(value) {
+            percentField.text = "\(percent)"
+        }
+    }
+    
+    @IBAction func tap8Button(_ sender: Any) {
+        let value = percentField.text! + "8"
+        if let percent = Int(value) {
+            percentField.text = "\(percent)"
+        }
+    }
+    
+    @IBAction func tap9Button(_ sender: Any) {
+        let value = percentField.text! + "9"
+        if let percent = Int(value) {
+            percentField.text = "\(percent)"
+        }
+    }
+    
+    @IBAction func tan0Button(_ sender: Any) {
+        let value = percentField.text! + "0"
+        if let percent = Int(value) {
+            percentField.text = "\(percent)"
+        }
+    }
+    
+    @IBAction func tapClearButton(_ sender: Any) {
+        percentField.text = "0"
+    }
 
     /*
     // MARK: - Navigation
