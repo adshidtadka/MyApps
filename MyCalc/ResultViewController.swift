b97c315de0213c5071d1c2f5c707fd1350ae72cc
adshidtadka <tsawa@icn.cce.i.kyoto-u.ac.jp>
2018-10-21 00:37:59 +0900

    add resultviewcontroller

---
+++ MyCalc/ResultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultViewController.swift
+//  MyCalc
+//
+//  Created by Sawa Takaaki on 2018/10/21.
+//  Copyright © 2018 Sawa Takaaki. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
